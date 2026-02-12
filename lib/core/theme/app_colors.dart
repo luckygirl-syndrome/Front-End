a167b578d23f617c27d3a1acf3719a920e1269a7
finger9999 <161580182+finger9999@users.noreply.github.com>
2026-02-12 11:30:45 +0900

    feat: font, color 추가

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color primary = Color(0xFFFFC63D); 
+  static const Color secondary = Color(0xFF80B0FF);
+
+  static const Color white0 = Color(0xFFFFFFFF);
+
+  static const Color black0 = Color(0xFF1A1A1A);
+
+  static const Color grey0 = Color(0xFFCCCCCC);
+}
---
+++ lib/core/theme/app_text_styles.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+// 이런식으로 쓰심 됩니다
+/*
+Text(
+  "준호님, 폰트 적용 완료입니다!",
+  style: AppTextStyles.ptdBold(20).copyWith(
+    color: AppColors.primary, // 아까 만든 컬러와 조합 가능!
+  ),
+)
+*/
+
+class AppTextStyles {
+  // pubspec.yaml에 등록한 family 이름과 똑같아야 합니다.
+  static const String _ptd = 'Pretendard';
+
+  // Pretendard - Thin (100)
+  static TextStyle ptdThin(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w100,
+  );
+
+  // Pretendard - Light (300)
+  static TextStyle ptdLight(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w300,
+  );
+
+  // Pretendard - Regular (400)
+  static TextStyle ptdRegular(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w400,
+  );
+
+  // Pretendard - Medium (500)
+  static TextStyle ptdMedium(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w500,
+  );
+
+  // Pretendard - SemiBold (600)
+  static TextStyle ptdSemiBold(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w600,
+  );
+
+  // Pretendard - Bold (700)
+  static TextStyle ptdBold(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w700,
+  );
+
+  // Pretendard - ExtraBold (800)
+  static TextStyle ptdExtraBold(double size) => TextStyle(
+    fontFamily: _ptd,
+    fontSize: size,
+    fontWeight: FontWeight.w800,
+  );
+}
